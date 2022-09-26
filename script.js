e08cc2097f7a89b0dad5dec273261d2d5dfb5ea3
Soufiane Fathaoui <soufianenakata7@gmail.com>
2022-09-27 00:57:59 +0100

    Add script for filtrable

---
+++ script.js
@@ -0,0 +1,26 @@
+const filterItem = document.querySelector(".items");
+const filterImage = document.querySelectorAll(".image");
+
+window.onload = () => { // once window loaded
+    filterItem.addEventListener("click", (selectedItem) => {
+        if(selectedItem.target.classList.contains("item"))
+        {
+            filterItem.querySelector(".active").classList.remove("active")
+            selectedItem.target.classList.add("active")
+
+            let selectedName = selectedItem.target.getAttribute("data-name")
+
+            filterImage.forEach((image) => {
+                let filterImg = image.getAttribute("data-name")
+                if (filterImg == selectedName || selectedName == "All") {
+                    image.classList.add("show")
+                    image.classList.remove("hide")
+                }
+                else{
+                    image.classList.add("hide")
+                }
+            })
+        }
+    })
+    
+}
