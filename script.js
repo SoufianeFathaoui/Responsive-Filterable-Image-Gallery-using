e9248d968d7ffc1d11c65b51e021fd90fe4e5ef5
Soufiane Fathaoui <soufianenakata7@gmail.com>
2022-09-27 01:00:38 +0100

    Last edit

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const filterItem = document.querySelector(".items");
 const filterImage = document.querySelectorAll(".image");
-
 window.onload = () => { // once window loaded
     filterItem.addEventListener("click", (selectedItem) => {
         if(selectedItem.target.classList.contains("item"))
         {
             filterItem.querySelector(".active").classList.remove("active")
             selectedItem.target.classList.add("active")
-
             let selectedName = selectedItem.target.getAttribute("data-name")
-
             filterImage.forEach((image) => {
                 let filterImg = image.getAttribute("data-name")
                 if (filterImg == selectedName || selectedName == "All") {
